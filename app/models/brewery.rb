3abed67a6be9265e22025a7393b3149fa7cf4350
Joni Mäkelä <joni.makela@gofore.com>
2016-01-31 19:01:02 +0200

    All done

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,6 @@
 class Brewery < ActiveRecord::Base
+  include RatingAverage
+
   has_many :beers
+  has_many :ratings, through: :beers
 end
---
+++ app/models/concerns/rating_average.rb
@@ -0,0 +1,12 @@
+module RatingAverage
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :ratings
+  end
+
+  def average_rating
+    ratings.average(:score)
+  end
+
+end
